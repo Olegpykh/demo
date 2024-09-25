adc5904ab68692f6d9ed1722fe708b0ba5cea46e
olegpykhonin <stoppard23@gmail.com>
2024-09-25 10:11:54 +0200

    Hallo

---
+++ src/homework_1/First.java
@@ -0,0 +1,20 @@
+package homework_1;
+
+public class First {
+
+        public static void main(String[] args) {
+        char a1 = 'G';
+        int a2 = 89;
+        byte a3 = 4;
+        short a4 =  56;
+        float a5 = 4.7333436F;
+        double a6 = 4.355453532;
+        long a7  = 12121;
+        long res = (long)(a1 + a2 + a3 +a4 +a5 +a6 + a7);
+        System.out.println(res);
+    }
+
+
+
+        }
+
