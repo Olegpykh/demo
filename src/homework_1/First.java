b37ad3d5872c6429c19ea6d2bb71e17a15aed970
Olegpykh <stoppard23@gmail.com>
2024-09-26 03:27:43 +0200

    homework25.09.2024.java

--- src/homework_1/First.java
+++ src/homework_1/First.java
@@ -1,20 +1,24 @@
-package homework_1;
+public class Homework {
+    public static void main(String[] args) {
 
-public class First {
-
-        public static void main(String[] args) {
         char a1 = 'G';
         int a2 = 89;
         byte a3 = 4;
-        short a4 =  56;
+        short a4 = 56;
         float a5 = 4.7333436F;
         double a6 = 4.355453532;
-        long a7  = 12121;
-        long res = (long)(a1 + a2 + a3 +a4 +a5 +a6 + a7);
-        System.out.println(res);
+        long a7 = 12121;
+        System.out.println("Character :" + a1);
+        System.out.println("Integer :" + a2);
+        System.out.println("Byte :" + a3);
+        System.out.println("Short :" + a4);
+        System.out.println("Float :" + a5);
+        System.out.println("Double :" + a6);
+        System.out.println("Long :" + a7);
+
+        short num = 345;
+        short num1 = 4;
+        short num2 = 5;
+        System.out.println("Число " + num + " > " + num / 115 + " , " + num1 + " , " + num2);
     }
-
-
-
-        }
-
+}
